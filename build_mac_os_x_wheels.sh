fb6e7eb0ca0b2ba6f952d475b4b2a44ad8296bf3
Sélim <selim@sqreen.io>
2019-05-28 19:59:57 +0200

    Use docker for building the wheels

---
+++ build_mac_os_x_wheels.sh
@@ -0,0 +1,16 @@
+set -e
+set -x
+
+for PYVERSION in 2.7 3.4 3.5 3.6 3.7; do
+    curl https://bootstrap.pypa.io/get-pip.py | python
+    pip install virtualenv
+    ORIG_PATH=$PATH
+    PATH=/Library/Frameworks/Python.framework/Versions/2.7/bin:$PATH
+    virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
+    PATH=$ORIG_PATH
+    . ./venv_${PYVERSION}/bin/activate
+    cd libinjection/python
+    make
+    python setup.py bdist_wheel --verbose
+    deactivate
+done
