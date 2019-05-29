29b80bdc9af8a5a830000d7da46ae9546a298f43
Sélim <selim@sqreen.io>
2019-05-29 10:19:14 +0200

    Correctly create wheel on mac os

--- build_mac_os_x_wheels.sh
+++ build_mac_os_x_wheels.sh
@@ -1,6 +1,7 @@
 set -e
 set -x
 
+cd libinjection/python
 for PYVERSION in 2.7 3.4 3.5 3.6 3.7; do
     curl https://bootstrap.pypa.io/get-pip.py | python
     pip install virtualenv
@@ -9,8 +10,9 @@ for PYVERSION in 2.7 3.4 3.5 3.6 3.7; do
     virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
     PATH=$ORIG_PATH
     . ./venv_${PYVERSION}/bin/activate
-    cd libinjection/python
     make
     python setup.py bdist_wheel --verbose
     deactivate
 done
+cd ../..
+cp -r libinjection/python/dist .
