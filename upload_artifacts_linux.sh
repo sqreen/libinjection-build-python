1b3ca4345f2d6ff28e62d2ad07cddb2b3bc695bb
Sélim <selim@sqreen.io>
2019-05-29 10:46:19 +0200

    Upload libinjection artifact

---
+++ upload_artifacts_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+set -x
+
+pip install --user awscli
+aws s3 cp dist/ "s3://sqreen-libinjection-travis-artefact/$(git rev-parse HEAD)/dist/" --recursive --exclude "*" --include "*.whl" --include "*.tar.gz"
