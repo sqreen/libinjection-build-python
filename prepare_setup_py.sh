88ca47f091141cf619499a4ff0038f442c4eaa26
Sélim <selim@sqreen.io>
2019-06-06 14:50:23 +0200

    Update sed command for mac

--- prepare_setup_py.sh
+++ prepare_setup_py.sh
@@ -5,6 +5,6 @@ set -e
 PACKAGE_NAME=libinjection-wheel
 VERSION="0.1.0"
 
-sed -i'.bak' -e "s/name\s\+=\s\+'libinjection'/name = '${PACKAGE_NAME}'/" libinjection/python/setup.py
-sed -i'.bak' -e "s/url\s\+=.\+,/url = 'https:\/\/github.com\/sqreen\/libinjection-build-python',/" libinjection/python/setup.py
-sed -i'.bak' -e "s/version\s\+=.\+,/version = '${VERSION}',/" libinjection/python/setup.py
+sed -i'.bak' -e "s/name\s\+=\s\+'libinjection'/name = '${PACKAGE_NAME}'/g" libinjection/python/setup.py
+sed -i'.bak' -e "s/url\s\+=.\+,/url = 'https:\/\/github.com\/sqreen\/libinjection-build-python',/g" libinjection/python/setup.py
+sed -i'.bak' -e "s/version\s\+=.\+,/version = '${VERSION}',/g" libinjection/python/setup.py
