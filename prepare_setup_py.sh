c74b15c3d246179dd168cfd8c1297c95d298ef68
Sélim <selim@sqreen.io>
2019-06-06 14:56:33 +0200

    Use perl instead of sed to be compatible with macos

--- prepare_setup_py.sh
+++ prepare_setup_py.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -e
+set -x
 
 PACKAGE_NAME=libinjection-wheel
 VERSION="0.1.0"
 
-sed -i'.bak' -e "s/name\s\+=\s\+'libinjection'/name = '${PACKAGE_NAME}'/g" libinjection/python/setup.py
-sed -i'.bak' -e "s/url\s\+=.\+,/url = 'https:\/\/github.com\/sqreen\/libinjection-build-python',/g" libinjection/python/setup.py
-sed -i'.bak' -e "s/version\s\+=.\+,/version = '${VERSION}',/g" libinjection/python/setup.py
+perl -i -pe"s/name\s+=\s+\'libinjection\'/name = \'${PACKAGE_NAME}\'/g" libinjection/python/setup.py
+perl -i -pe"s/url\s+=.+,/url = \'https:\/\/github.com\/sqreen\/libinjection-build-python\',/g" libinjection/python/setup.py
+perl -i -pe"s/version\s+=.+,/version = '${VERSION}',/g" libinjection/python/setup.py
