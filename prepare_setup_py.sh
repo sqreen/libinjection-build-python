32d868d98894fab3c512de8bb21639d805b0e770
Sélim <selim@sqreen.io>
2019-06-06 14:31:14 +0200

    Fix sed on macos

--- prepare_setup_py.sh
+++ prepare_setup_py.sh
@@ -3,7 +3,6 @@
 set -e
 
 PACKAGE_NAME=libinjection-wheel
-URL=
 
-sed -i "s/name\s\+=\s\+'libinjection'/name = '${PACKAGE_NAME}'/" libinjection/python/setup.py
-sed -i "s/url\s\+=.\+,/url = 'https:\/\/github.com\/sqreen\/libinjection-build-python',/" libinjection/python/setup.py
+sed -i'.bak' -e "s/name\s\+=\s\+'libinjection'/name = '${PACKAGE_NAME}'/" libinjection/python/setup.py
+sed -i'.bak' -e "s/url\s\+=.\+,/url = 'https:\/\/github.com\/sqreen\/libinjection-build-python',/" libinjection/python/setup.py
