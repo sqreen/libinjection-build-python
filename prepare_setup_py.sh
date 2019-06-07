4b6ba62c6bb6a5c9d353315d535b13c0d00fddbf
Jānis Kiršteins <janis@sqreen.com>
2019-06-07 14:57:07 +0200

    Version bump to 0.1.1
    This should work on macOS

--- prepare_setup_py.sh
+++ prepare_setup_py.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 PACKAGE_NAME=libinjection-wheel
-VERSION="0.1.0"
+VERSION="0.1.1"
 
 perl -i -pe"s/name\s+=\s+\'libinjection\'/name = \'${PACKAGE_NAME}\'/g" libinjection/python/setup.py
 perl -i -pe"s/url\s+=.+,/url = \'https:\/\/github.com\/sqreen\/libinjection-build-python\',/g" libinjection/python/setup.py
